4ce14ec674e350a467c510994eabc4939c108a8d
lance <zibranaby069@gmail.com>
2025-11-10 09:00:00 +0700

    feat(pages) adding a few page that my used latter

---
+++ lib/Screens/Widgets/mainGrid/payement_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PayementPage extends StatelessWidget {
+  const PayementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
