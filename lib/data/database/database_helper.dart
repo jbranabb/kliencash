0e216e9f10fb060458f634b18d8a4fc72bb3559b
jbranabb <zibranaby069@gmail.com>
2025-11-12 10:34:54 +0700

    feat(database_helper) adding new table

--- lib/data/database/database_helper.dart
+++ lib/data/database/database_helper.dart
@@ -26,7 +26,49 @@ class DatabaseHelper {
       name TEXT NOT NULL,
       handphone TEXT NOT NULL,
       country_code TEXT NOT NULL,
-      alamat TEXT NOT NULL)
+      alamat TEXT NOT NULL) 
       ''');
+
+    await db.execute('''
+    CREATE TABLE PROJECTS(
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER NOT NULL,
+    agenda TEXT NOT NULL,
+    status TEXT NOT NULL,
+    createdAt TEXT NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES CLIENT(Id) ON DELETE CASCADE
+    )
+    ''');
+
+    await db.execute('''
+    CREATE TABLE INVOICE(
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_id INTEGER NOT NULL,
+    status TEXT NOT NULL,
+    subtotal REAL NOT NULL,
+    pajak REAL NOT NULL,
+    discount REAL NOT NULL,
+    total_amount REAL NOT NULL,
+    tanggal TEXT NOT NULL,    
+    jatuh_tempo TEXT NOT NULL,    
+    createdAt TEXT NOT NULL,
+    invoice_number TEXT NOT NULL UNIQUE,
+    notes TEXT,
+    FOREIGN KEY (project_id) REFERENCES PROJECTS(Id) ON DELETE CASCADE
+    )
+      ''');
+
+    await db.execute('''
+    CREATE TABLE PAYMENT(
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    invoice_id INTEGER NOT NULL,
+    amount REAL NOT NULL,
+    tanggal_bayar TEXT NOT NULL,
+    payment_method TEXT NOT NULL,
+    bukti_payment TEXT NOT NULL,
+    notes TEXT,
+    FOREIGN KEY (invoice_id) REFERENCES INVOICE(Id) ON DELETE CASCADE
+    )
+    ''');
   }
 }
