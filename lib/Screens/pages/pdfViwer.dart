d1473e076db2998a4606040fba3cfe3fa0c59ad4
jbranabb <zibranaby069@gmail.com>
2025-11-18 20:00:00 +0700

    feat(pdfViewer)adding pdfname file base on invoice number and projects agenda

--- lib/Screens/pages/pdfViwer.dart
+++ lib/Screens/pages/pdfViwer.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kliencash/Screens/Widgets/appbar.dart';
 import 'package:kliencash/Screens/pages/pdf.dart';
+import 'package:kliencash/state/cubit/selectedInvoice.dart';
 import 'package:printing/printing.dart';
+
 class Pdfviwer extends StatelessWidget {
   const Pdfviwer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var state = context.read<Selectedinvoice>().state;
+    var invoice = state[0];
     return Scaffold(
       appBar: myAppBar(context, 'Invoice PDF Preview'),
       body: PdfPreview(
-        build: (format) => generatePDF(context) ,
+        allowPrinting: true,
+        canDebug: false,
+        pdfFileName: '${invoice.invoiceNumber}-${invoice.projectsModel!.agenda}',
+        build: (format) => generatePDF(context),
       ),
     );
   }
