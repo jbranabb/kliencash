dc2f5f77e6d9dcde0a5c0035ee7a8ae92ffa56e9
jbranabb <zibranaby069@gmail.com>
2025-11-30 07:43:17 +0700

    refactor(main_grid) refactoring the layout

--- lib/Screens/Widgets/main_grid.dart
+++ lib/Screens/Widgets/main_grid.dart
@@ -5,6 +5,7 @@ import 'package:kliencash/Screens/pages/mainGrid/invoice/invoice_page.dart';
 import 'package:kliencash/Screens/pages/mainGrid/operasional/operasional_page.dart';
 import 'package:kliencash/Screens/pages/mainGrid/payment/payment_page.dart';
 import 'package:kliencash/Screens/pages/mainGrid/project/projects_page.dart';
+
 SliverToBoxAdapter mainGrid() {
   return SliverToBoxAdapter(
     child: SizedBox(
@@ -23,27 +24,41 @@ SliverToBoxAdapter mainGrid() {
             child: InkWell(
               borderRadius: BorderRadius.circular(20),
               onTap: () {
-                    if(index == 0){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ClientPage(),));
-                    }
-                    else if(index == 1){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ProjectsPage(),));
-                    }
-                    else if(index == 2){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const InvoicePage(),));
-                    }
-                    else if(index == 3){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const PayementPage(),));
-                    }
-                    else if(index == 4){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const OperasionalPage(),));
-                    }
-                  },
+                if (index == 0) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const ClientPage()),
+                  );
+                } else if (index == 1) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ProjectsPage(),
+                    ),
+                  );
+                } else if (index == 2) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const OperasionalPage(),
+                    ),
+                  );
+                } else if (index == 3) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const InvoicePage(),
+                    ),
+                  );
+                } else if (index == 4) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const PayementPage(),
+                    ),
+                  );
+                }
+              },
               child: Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
-                 color: Colors.white,
+                  color: Colors.white,
                   boxShadow: [
                     BoxShadow(
                       offset: Offset(0.2, 4),
@@ -52,7 +67,7 @@ SliverToBoxAdapter mainGrid() {
                     ),
                   ],
                 ),
-                child: childGrid(index,context),
+                child: childGrid(index, context),
               ),
             ),
           ),
@@ -64,68 +79,96 @@ SliverToBoxAdapter mainGrid() {
 
 Widget childGrid(int index, BuildContext context) {
   switch (index) {
-    case 0: 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.person),
-        MyText(title: "Client")
-      ],
-    );
+    case 0:
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.person,
+          ),
+          MyText(title: "Client"),
+        ],
+      );
     case 1:
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.work),
-        MyText(title: "Projects")
-      ],
-    );
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.work,
+          ),
+          MyText(title: "Projects"),
+        ],
+      );
     case 2:
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-       Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.receipt_long_rounded),
-        MyText(title: "Invoice")
-       ],
-    );
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.factory,
+          ),
+          MyText(title: "Operasional", textAlign: TextAlign.center),
+        ],
+      );
     case 3:
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.attach_money_rounded),
-        MyText(title: "Payment")
-      ],
-    );
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.receipt_long_rounded,
+          ),
+          MyText(title: "Invoice"),
+        ],
+      );
     case 4:
       return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.factory),
-        MyText(title: "Operasional", textAlign: TextAlign.center,)
-      ],
-    );
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.attach_money_rounded,
+          ),
+          MyText(title: "Payment"),
+          ],
+      );
     case 5:
       return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.bar_chart_rounded),
-        MyText(title: "Report")
-      ],
-    );
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.bar_chart_rounded,
+          ),
+          MyText(title: "Report"),
+        ],
+      );
     default:
       return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(color: Theme.of(context).colorScheme.onPrimary,size: 30, Icons.person),
-        MyText(title: "seprei gratis bowok", textAlign: TextAlign.center,)
-      ],
-    );
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            color: Theme.of(context).colorScheme.onPrimary,
+            size: 30,
+            Icons.person,
+          ),
+          MyText(title: "seprei gratis bowok", textAlign: TextAlign.center),
+        ],
+      );
   }
 }
