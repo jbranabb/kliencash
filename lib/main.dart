9f102c10dbeb0801e9bc6b9b626551287e1e2580
jbranabb <zibranaby069@gmail.com>
2025-12-05 16:12:37 +0700

    feat(main) settup local package in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
-
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:kliencash/Screens/Widgets/picked_pict.dart';
 import 'package:kliencash/Screens/pages/home.dart';
 import 'package:kliencash/Screens/pages/intialpage/onBorading.dart';
@@ -33,6 +31,7 @@ import 'package:kliencash/state/cubit/selectedInvoice.dart';
 import 'package:kliencash/state/cubit/selectedProjects.dart';
 import 'package:kliencash/state/cubit/selectedpaymentMethod.dart';
 import 'package:kliencash/state/cubit/statusProjectrs.dart';
+import 'package:kliencash/state/cubit/toggleLang.dart';
 import 'package:kliencash/themeData.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -40,50 +39,65 @@ SharedPreferences? pref;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences initialpref = await SharedPreferences.getInstance();
+  await EasyLocalization.ensureInitialized();
   pref = initialpref;
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
   Bloc.observer = Observer();
-  int launchCount  = pref?.getInt('launchCount') ?? 0;
+  int launchCount = pref?.getInt('launchCount') ?? 0;
   await pref?.setInt('launchCount', launchCount + 1);
   print('launchCount :$launchCount');
   runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => ClientBloc()..add(ReadDataClient())),
-        BlocProvider(create: (context) => CountrycodeCubit()),
-        BlocProvider(create: (context) => StatusprojectrsCubit()),
-        BlocProvider(create: (context) => Selectedclient()),
-        BlocProvider(create: (context) => Selecteddatecubit()),
-        BlocProvider(create: (context) => ProjectsBloc()..add(ReadDataProjects()),),
-        BlocProvider(create: (context) => SelectedProjects()),
-        BlocProvider(create: (context) => CountMount()),
-        BlocProvider(create: (context) => CheckbookPembulatan()),
-        BlocProvider(create: (context) => DropDownRounded()),
-        BlocProvider(create: (context) => DropdownStatusinvoice()),
-        BlocProvider(create: (context) => InvoiceBloc()..add(ReadInvoice())),
-        BlocProvider(create: (context) => Selectedinvoice()),
-        BlocProvider(create: (context) => OpenBahasaToggle()),
-        BlocProvider(create: (context) => OpenThemeToggle()),
-        BlocProvider(create: (context) => ChangeTheme()..initialze()),
-        BlocProvider(create: (context) => Onboardingcubit()),
-        BlocProvider(create: (context) => UsersBloc()..add(ReadDataUsers())),
-        BlocProvider(create: (context) => TogglePaymentMethod()),
-        BlocProvider(create: (context) => PaymentMethodBloc()..add(ReadPaymentMethod())),
-        BlocProvider(create: (context) => SelectedPaymentMethod()),
-        BlocProvider(create: (context) => BookstatuslengthCubit()..getlength()),
-        BlocProvider(create: (context) => PickedPict()),
-        BlocProvider(create: (context) => SelectDateAddPayement()),
-        BlocProvider(create: (context) => PaymentBloc()),
-        BlocProvider(create: (context) => OperasionalBloc()..add(ReadData())),
-        BlocProvider(create: (context) => ChartDataClientCubit()),
-        BlocProvider(create: (context) => ChartProjectsStaus()),
-        BlocProvider(create: (context) => ChartProjectsHightValue()),
-        BlocProvider(create: (context) => ChartinvoiceStatus()),
-      ],
-      child: MainApp(launchcount: launchCount,),
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('id')],
+      path: 'assets/lang',
+      fallbackLocale: Locale('en'),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => ClientBloc()..add(ReadDataClient()),
+          ),
+          BlocProvider(create: (context) => CountrycodeCubit()),
+          BlocProvider(create: (context) => StatusprojectrsCubit()),
+          BlocProvider(create: (context) => Selectedclient()),
+          BlocProvider(create: (context) => Selecteddatecubit()),
+          BlocProvider(
+            create: (context) => ProjectsBloc()..add(ReadDataProjects()),
+          ),
+          BlocProvider(create: (context) => SelectedProjects()),
+          BlocProvider(create: (context) => CountMount()),
+          BlocProvider(create: (context) => CheckbookPembulatan()),
+          BlocProvider(create: (context) => DropDownRounded()),
+          BlocProvider(create: (context) => DropdownStatusinvoice()),
+          BlocProvider(create: (context) => InvoiceBloc()..add(ReadInvoice())),
+          BlocProvider(create: (context) => Selectedinvoice()),
+          BlocProvider(create: (context) => OpenBahasaToggle()),
+          BlocProvider(create: (context) => OpenThemeToggle()),
+          BlocProvider(create: (context) => ChangeTheme()..initialze()),
+          BlocProvider(create: (context) => Onboardingcubit()),
+          BlocProvider(create: (context) => UsersBloc()..add(ReadDataUsers())),
+          BlocProvider(create: (context) => TogglePaymentMethod()),
+          BlocProvider(
+            create: (context) => PaymentMethodBloc()..add(ReadPaymentMethod()),
+          ),
+          BlocProvider(create: (context) => SelectedPaymentMethod()),
+          BlocProvider(
+            create: (context) => BookstatuslengthCubit()..getlength(),
+          ),
+          BlocProvider(create: (context) => PickedPict()),
+          BlocProvider(create: (context) => SelectDateAddPayement()),
+          BlocProvider(create: (context) => PaymentBloc()),
+          BlocProvider(create: (context) => OperasionalBloc()..add(ReadData())),
+          BlocProvider(create: (context) => ChartDataClientCubit()),
+          BlocProvider(create: (context) => ChartProjectsStaus()),
+          BlocProvider(create: (context) => ChartProjectsHightValue()),
+          BlocProvider(create: (context) => ChartinvoiceStatus()),
+          BlocProvider(create: (context) => Togglelang()),
+        ],
+        child: MainApp(launchcount: launchCount),
+      ),
     ),
   );
 }
@@ -91,12 +105,19 @@ void main() async {
 class MainApp extends StatelessWidget {
   MainApp({super.key, required this.launchcount});
   int launchcount;
-   @override
+  @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<Togglelang>().nowlangue(context.locale.languageCode);
+    });
     print(launchcount);
+    print('locale :${context.locale.languageCode}');
     return BlocBuilder<ChangeTheme, int>(
       builder: (context, state) {
         return MaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
           debugShowCheckedModeBanner: false,
           theme: state == 0
               ? normalTheme
